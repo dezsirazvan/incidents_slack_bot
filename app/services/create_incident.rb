2125f2535e8a67f50f874e9ae7ac095daf278737
Răzvicul <razvicul@Bias-MacBook-Pro.local>
2023-05-11 16:03:08 +0200

    Fetch correct the team id

--- app/services/create_incident.rb
+++ app/services/create_incident.rb
@@ -8,9 +8,9 @@ class CreateIncident
 
   def call
     # Parse the title, description and severity from the dialog submission
-    title = @data['submission']['title']
-    description = @data['submission']['description']
-    severity = @data['submission']['severity']
+    title = @data.dig('submission', 'title')
+    description = @data.dig('submission', 'description')
+    severity = @data.dig('submission', 'severity')
 
     # Create a new Slack channel for the incident
     channel_name = "incident-#{title}-#{Time.now.strftime('%F-%H-%M')}"
@@ -20,23 +20,23 @@ class CreateIncident
       title: title,
       description: description,
       severity: severity,
-      creator: @data['user']['name'],
+      creator: @data.dig('user', 'name'),
       status: 'open',
       channel_id: channel['id']
     )
 
     # Invite the creator in the new channel
-    client.conversations_invite(channel: channel['id'], users: @data['user']['id'])
+    client.conversations_invite(channel: channel['id'], users: @data.dig('user', 'id'))
 
     # Send a message to the incident channel with the details of the incident
     client.chat_postMessage(
       channel: channel['id'],
-      text: "New incident declared by <@#{@data['user']['name']}>:\nTitle: #{title}\nDescription: #{description}\nSeverity: #{severity}"
+      text: "New incident declared by <@#{@data.dig('user', 'name')}>:\nTitle: #{title}\nDescription: #{description}\nSeverity: #{severity}"
     )
 
     # Send a confirmation message to the user
     client.chat_postMessage(
-      channel: @data['channel']['id'],
+      channel: @data.dig('channel', 'id'),
       text: "Incident created in channel <##{channel['id']}>"
     )
   rescue StandardError => e
@@ -46,6 +46,6 @@ class CreateIncident
   private
 
   def client
-    @client ||= SlackClient.client(@data['team_id'])
+    @client ||= SlackClient.client(@data.dig('team', 'id'))
   end
 end
--- lib/slack_client.rb
+++ lib/slack_client.rb
@@ -2,7 +2,7 @@ require 'slack-ruby-client'
 
 module SlackClient
   def self.client(team_id=nil)
-    team = Team.find_by(team_id: team_id)
+    team = Team.where(team_id: team_id)&.last
     token = team&.token || ENV['SLACK_API_TOKEN']
 
     @client ||= Slack::Web::Client.new(token: token)
